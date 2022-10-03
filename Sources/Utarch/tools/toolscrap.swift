f091c85628f7dfc588ef3243b2187f52d89696aa
Luciana Adriao <luicianaasantos@hotmail.com>
2022-10-03 13:45:43 -0300

    Indentation fix

--- Sources/Utarch/tools/toolscrap.swift
+++ Sources/Utarch/tools/toolscrap.swift
@@ -6,13 +6,13 @@ struct ToolScrap{
     init(link: String){
         self.link = link
     }
-    
+
     func getContent() -> String {
         let url = URL(string: self.link)!
         let htmlInString = try! String(contentsOf: url)
         return htmlInString
     }
-    
+
     func getHtml() -> Element{
         let dom = try! SwiftSoup.parse(self.getContent())
         return dom
